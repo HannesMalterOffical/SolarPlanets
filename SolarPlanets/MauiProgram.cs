18ec0682827d00cf20ebd85125e76dee7ef500a5
HannesMalterOffical <hannesmlater1234@gmail.com>
2023-03-15 15:46:51 +0200

    Add project files.

---
+++ SolarPlanets/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace SolarPlanets;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("", "RegularFont");
+				fonts.AddFont("", "MedoiumFont");
+                fonts.AddFont("", "BoldFont");
+            });
+
+		return builder.Build();
+	}
+}
