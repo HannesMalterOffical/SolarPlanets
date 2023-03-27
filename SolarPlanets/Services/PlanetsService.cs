d5535de3cb01738f7b897f4dd55095135b6c1b0f
Hannes <Hannesmalter1234@gmail.com>
2023-03-27 15:05:00 +0300

    Added Mercury and other stuff 🌹

---
+++ SolarPlanets/Services/PlanetsService.cs
@@ -0,0 +1,39 @@
+﻿using SolarPlanets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarPlanets.Services
+{
+    internal static class PlanetsService
+    {
+        private static List<Planet> planets = new()
+        {
+            new()
+            {
+                Name = "Mercury",
+                Subtitle = "The smallest planet",
+                HeroImage = "mercury.png",
+                Description = "Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth. It is the smallest planet in the Solar System, with an equatorial radius of 2,439.7 kilometres (1,516.0 mi). The ancients knew Mercury by different names depending on whether it was an evening star or a morning star. By about 350 BC, the ancient Greeks had realized the two stars were one. They knew the planet as Στίλβων Stilbōn, meaning \"twinkling\"."
+                AccentColorStart = Color.FromArgb("#353535"),
+                AccentColorEnd = Color.FromArgb("#8d9098"),
+                Images = new()
+                {
+                    "https://www.spaceviews.de/wp-content/uploads/2020/01/MercuryTitel.png",
+                    "https://www.dlr.de/dlr/Portaldata/1/Resources/bilder/portal/portal_2011_3/merkur1.jpg"
+                }
+            }
+        };
+
+
+        public static List<Planet> GetFeaturedPlanets()
+        {
+            var rnd = new Random();
+            var randomizedPlanets = planets.OrderBy(Item => rnd.Next());
+
+            return randomizedPlanets.Take(2).ToList();
+        }
+    }
+}
--- SolarPlanets/Views/PlanetsPage.xaml.cs
+++ SolarPlanets/Views/PlanetsPage.xaml.cs
@@ -1,4 +1,5 @@
-namespace SolarPlanets.Views;
+using SolarPlanets.Models;
+using SolarPlanets.Services;
 
 public partial class PlanetsPage : ContentPage
 {
@@ -7,6 +8,13 @@ public partial class PlanetsPage : ContentPage
 	public PlanetsPage()
 	{
 		InitializeComponent();
+
+		lstPopularPlanets.ItemSource = PlanetsService.GetFeaturedPlanets();
+	}
+
+	async void Planets_SelectionChanges(System.Object sender, System.EventArgs e)
+	{
+		await Navigation.PushAsync(new PlanetDetailsPage(e CurrentSelection.First() as Planet));
 	}
 
 	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
@@ -21,4 +29,11 @@ public partial class PlanetsPage : ContentPage
         _ = MainContentGrid.ScaleTo(1, AnimationDuration);
         await MainContentGrid.TranslateTo(0,0, AnimationDuration, Easing.CubicIn);
     }
+
+	async void ProfilePicture_Clicked(System.Object sender, System,EventArgs e)
+	{
+		_ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+		await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+		_ = MainContentGrid.FadeTo(0.8, AnimationDuration);
+	}
 }
